a02e35b02c61c27f5b82738adc08093f86710410
Michal Švarc <michal.svarc@surquest.com>
2024-03-06 21:32:06 +0100

    List of schools - draft

---
+++ src/js/var.config.js
@@ -0,0 +1,10 @@
+var config = {
+    survey: {
+        name: 'Surquest - Survey',
+        link: 'https://www.survio.com/survey/d/I3E8L1I3K6E3G6G',
+    },
+    contact: {
+        name: 'Michal Švarc',
+        email: 'michal.svarc@surquest.com'
+    }
+}
