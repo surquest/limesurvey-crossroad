a514698cf9b5314183e0092cf27b90de0dad40f6
Michal Švarc <michal.svarc@surquest.com>
2024-03-06 21:00:21 +0100

    First draft of the crossroad

---
+++ src/js/var.schools.js
@@ -0,0 +1,11 @@
+const optionsSchools = [
+    { id: '1', title: 'School 1' },
+    { id: '2', title: 'School 2' },
+    { id: '3', title: 'School 3' },
+    { id: '4', title: 'School 4' },
+    { id: '5', title: 'School 5' },
+    { id: '6', title: 'School 6' },
+    { id: '7', title: 'School 7' },
+    { id: '8', title: 'School 8' },
+    { id: '9', title: 'School 9' }
+]
